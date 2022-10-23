f74c88e15c5dbff4413fa38e130772388416b274
dieguito <diegopezzano20@gmail.com>
2022-10-23 09:31:38 -0300

    ingreso de datos 23/10

---
+++ calculos.php
@@ -0,0 +1,8 @@
+<?php
+$A= $_POST['nro1'] ?? 0;
+$B= $_POST['nro2'] ?? 0;
+echo "$A elevado al cuadrado es : " . $A**2 . PHP_EOL . "<br>";
+echo "$B elevado al cuadrado es : " . $B**2 . PHP_EOL . "<br>";
+
+
+?>
---
+++ readline.php
@@ -0,0 +1,19 @@
+// metodos para enviar archivos GET Y POST  
+
+// condicionales IF ELSE  ELSEIF  SWITCH
+
+// operador ternario   ?   : 
+
+// operador null coalesce   ?? 
+
+<?php
+$A= readline('ingrese el valor de a :');
+$B= readline('ingrese el valor de b :');
+
+echo "$A elevado al cuadrado es : " . $A**2 . PHP_EOL;
+echo "$B elevado al cuadrado es : " . $B**2 . PHP_EOL;
+
+
+
+
+?>
